e09badf624e8fcaafbdc90354caf1b1799b67c18
Ibrahim Hossain <ibuhossain3@gmail.com>
2023-05-26 23:42:56 +0600

    login page update

--- src/main/java/com/example/hellofx/Login.java
+++ src/main/java/com/example/hellofx/Login.java
@@ -10,6 +10,9 @@ import java.util.Objects;
 
 public class Login extends Application {
 
+    private static final int INIT_WIDTH = 840;
+    private static final int INIT_HEIGHT = 580;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -18,7 +21,10 @@ public class Login extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
         primaryStage.setTitle("Train Ticket Booking System");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, INIT_WIDTH+100, INIT_HEIGHT+100));
+        primaryStage.setMinWidth(INIT_WIDTH);
+        primaryStage.setMinHeight(INIT_HEIGHT);
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 }
--- src/main/java/com/example/hellofx/LoginController.java
+++ src/main/java/com/example/hellofx/LoginController.java
@@ -1,16 +1,26 @@
 package com.example.hellofx;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
 
     @FXML
+    private AnchorPane anchorMainContainer;
+    @FXML
+    private VBox subContainer;
+    @FXML
     private TextField emailTextField;
-
     @FXML
     private PasswordField passwordField;
 
@@ -26,11 +36,34 @@ public class LoginController {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Information");
         alert.setHeaderText(null);
-        if (email.length() > 0 && password.length() > 0)
-            alert.setContentText("Login Successful!");
-        else
-            alert.setContentText("Invalid username and password!");
+        if (email.length() > 0 && password.length() > 0) alert.setContentText("Login Successful!");
+        else alert.setContentText("Invalid username and password!");
         alert.showAndWait();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(this::centerChildPane);
+
+        anchorMainContainer.widthProperty().addListener((observable, oldValue, newValue) -> {
+            centerChildPane();
+        });
+
+        anchorMainContainer.heightProperty().addListener((observable, oldValue, newValue) -> {
+            centerChildPane();
+        });
+    }
+
+    private void centerChildPane() {
+        double paneWidth = anchorMainContainer.getWidth();
+        double paneHeight = anchorMainContainer.getHeight();
+        double childWidth = subContainer.getWidth();
+        double childHeight = subContainer.getHeight();
+
+        double offsetX = (paneWidth - childWidth) / 2;
+        double offsetY = (paneHeight - childHeight) / 2;
+
+        AnchorPane.setLeftAnchor(subContainer, offsetX);
+        AnchorPane.setTopAnchor(subContainer, offsetY);
+    }
 }
